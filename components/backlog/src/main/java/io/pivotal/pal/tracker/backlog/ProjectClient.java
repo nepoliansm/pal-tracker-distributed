dfeec2dc504dbcd6de8e25ce0abacddb27e18c3c
Nepolian <nepolian.marimuthu@wipro.com>
2019-04-11 04:17:17 -0700

    hystrix cmd enabled

--- components/backlog/src/main/java/io/pivotal/pal/tracker/backlog/ProjectClient.java
+++ components/backlog/src/main/java/io/pivotal/pal/tracker/backlog/ProjectClient.java
@@ -1,9 +1,13 @@
 package io.pivotal.pal.tracker.backlog;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.client.RestOperations;
 
-public class ProjectClient {
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
+public class ProjectClient {
+    private final Map<Long, ProjectInfo> projectsCache = new ConcurrentHashMap<Long, ProjectInfo>();
     private final RestOperations restOperations;
     private final String endpoint;
 
@@ -12,7 +16,15 @@ public class ProjectClient {
         this.endpoint = registrationServerEndpoint;
     }
 
+    @HystrixCommand(fallbackMethod = "getProjectFromCache")
     public ProjectInfo getProject(long projectId) {
-        return restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
+        ProjectInfo projectInfo = restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
+        projectsCache.put(projectId, projectInfo);
+
+        return projectInfo;
+    }
+
+    public ProjectInfo getProjectFromCache(long projectId) {
+        return projectsCache.get(projectId);
     }
 }
